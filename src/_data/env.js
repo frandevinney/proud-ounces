46b6d4c65867594f0c87c733b4f355d415a4797f
Fran Devinney <fdevinney@gmail.com>
2021-01-19 20:29:02 -0500

    update site variables

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,8 +1,3 @@
-const environment = process.env.ELEVENTY_ENV;
-const PROD_ENV = 'production';
-const isProd = environment === PROD_ENV;
-
 module.exports = {
-  environment,
-  isProd
+  environment: process.env.ELEVENTY_ENV
 };
